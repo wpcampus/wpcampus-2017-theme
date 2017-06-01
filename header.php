d662f0267a7439102f77721c77c02181d36d8ffa
Rachel Carden <Rachel.Carden@disney.com>
2017-06-01 09:44:49 -0700

    Add survey callout

--- header.php
+++ header.php
@@ -121,3 +121,12 @@ $tweets_tagboard = 'https://tagboard.com/wpcampus/300756';
 					if ( ! is_page( 'map' ) ) {
 						echo wpcampus_get_breadcrumbs_html();
 					}
+
+					// Don't add to tickets section or blog post.
+					if ( ! is_page( 'tickets' ) && ! is_single( 'wordpress-in-education-survey' ) ) :
+
+						?>
+						<div class="callout" style="text-align:center;"><h2>The "WordPress in Education" Survey</h2><p>After an overwhelming response to our 2016 survey, WPCampus is back this year to dig a little deeper on key topics that schools and campuses care about most when it comes to WordPress and website development. We’d love to include your feedback in our results this year. The larger the data set, the more we all benefit. <strong>The survey will close on June 23rd, 2017.</strong></p><a class="button block" style="color:#fff;margin-bottom:0;" href="https://2017.wpcampus.org/announcements/wordpress-in-education-survey/">Take the "WordPress in Education" survey</a></div>
+						<?php
+
+					endif;
