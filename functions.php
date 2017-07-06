3ad2344d5e3852ed8a2b86f255f4943c2d7eeda2
Rachel Carden <Rachel.Carden@disney.com>
2017-07-06 13:04:07 -0700

    Adding light blue callout style

--- functions.php
+++ functions.php
@@ -57,7 +57,7 @@ add_action( 'widgets_init', 'wpc_2017_register_sidebars' );
  * Setup styles and scripts.
  */
 function wpc_2017_enqueue_scripts() {
-	$wpcampus_version = '0.24';
+	$wpcampus_version = '0.26';
 
 	// Get the directory.
 	$wpcampus_dir = trailingslashit( get_stylesheet_directory_uri() );
