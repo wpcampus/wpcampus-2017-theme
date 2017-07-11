5a2be6c355eb0558b4e3ee255e644c19ba00592c
Rachel Carden <Rachel.Carden@disney.com>
2017-07-11 10:29:07 -0700

    Styling sponsor posts in undergrad blog post

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@ add_action( 'widgets_init', 'wpc_2017_register_sidebars' );
  * Setup styles and scripts.
  */
 function wpc_2017_enqueue_scripts() {
-	$wpcampus_version = '0.27';
+	$wpcampus_version = '0.28';
 
 	// Get the directory.
 	$wpcampus_dir = trailingslashit( get_stylesheet_directory_uri() );
